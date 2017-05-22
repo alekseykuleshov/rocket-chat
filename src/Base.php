549bfeb0995087d01829b56a1daed651088c37cb
Aleksey Kuleshov <alexeykuleshov@gmail.com>
2017-05-22 17:46:12 +0300

    doing requests via guzzle 3

--- src/Base.php
+++ src/Base.php
@@ -8,7 +8,11 @@ abstract class Base {
 
 	public static function init($instance, $root) {
 
-		self::$client = new \GuzzleHttp\Client(['base_uri' => $instance . $root]);
+		// Guzzle 6
+		//self::$client = new \GuzzleHttp\Client(['base_uri' => $instance . $root]);
+
+		// Guzzle 3
+		self::$client = new \Guzzle\Http\Client($instance . $root);
 	}
 
 	protected static function setAuthUserId($userId) {
@@ -21,6 +25,59 @@ abstract class Base {
 		self::$authToken = $authToken;
 	}
 
+	// Guzzle 3
+	protected static function send($url, $method = "GET", $data = null) {
+
+		if ( empty(self::$client) ) {
+
+			throw new Exception("You should init first");
+		}
+
+		// Default request parameters
+		$params = array("timeout" => 60, "connect_timeout" => 60, "exceptions" => false);
+
+		// Set authorization headers
+		$headers = array();
+		if ( ! empty(self::$authUserId) ) {
+
+			$headers["X-User-Id"] = self::$authUserId;
+		}
+		if ( ! empty(self::$authToken) ) {
+
+			$headers["X-Auth-Token"] = self::$authToken;
+		}
+
+		if ( ! empty($headers) ) {
+
+			$params["headers"] = $headers;
+		}
+
+		// Set data
+		if ( ( $method == "GET" ) && ( ! empty($data) ) ) {
+
+			$request = self::$client->get($url . "?" . http_build_query($data), $headers);
+		}
+		if ( ( $method == "POST" ) && ( ! empty($data) ) ) {
+
+			$request = self::$client->post($url, $headers, json_encode($data));
+		}
+
+		// Do request
+		$res = $request->send();
+
+		$code = $res->getStatusCode();
+		$body = $res->getBody();
+
+		if ( ( $code >= 200 ) && ($code < 300) ) {
+
+			return json_decode($body);
+		} else {
+
+			return false;
+		}
+    }
+
+	/* Guzzle 6
 	protected static function send($url, $method = "GET", $data = null) {
 
 		if ( empty(self::$client) ) {
@@ -74,5 +131,5 @@ abstract class Base {
 
 			return false;
 		}
-    }
+    } */
 }
