4986c617d1f388440b3770a95af3a1d09988d610
Aleksey Kuleshov <alexeykuleshov@gmail.com>
2017-05-22 11:55:54 +0300

    ability to create and update users

--- src/User.php
+++ src/User.php
@@ -3,17 +3,27 @@
 class User extends Base {
 
 	private $id;
-	private $name;
+	private $userName;
 	private $email;
+	private $name;
+	private $password;
 
-	public function __construct($userId) {
+	private $active;
+	private $roles;
+	private $joinDefaultChannels;
+	private $requirePasswordChange;
+	private $sendWelcomeEmail;
+	private $verified;
+	private $customFields;
+
+	public function __construct($userId = null) {
 
 		$this->id = $userId;
 	}
 
-	static public function login($name, $password, $auth = true) {
+	static public function login($userName, $password, $auth = true) {
 
-		$user = self::send("login", "POST", array("user" => $name, "password" => $password));
+		$user = self::send("login", "POST", array("user" => $userName, "password" => $password));
 
 		if ( ! $user ) {
 
@@ -47,10 +57,65 @@ class User extends Base {
 		}
 
 		$userData = new self($user->_id);
-		$userData->name = $user->name;
-		$userData->email = $user->emails[0]->address;
+		$userData->setName($user->name);
+		$userData->setUserName($user->username);
+
+		if ( ! empty($user->emails) ) {
+
+			$userData->setEmail($user->emails[0]->address);
+		}
 
 		return $userData;
+	}
+
+	public function create() {
+
+		$userData = getUserData();
+
+		$user = self::send("users.create", "POST", $userData);
+
+		if ( ! $user ) {
+
+			return false;
+		}
+
+		$this->id = $user->user->_id;
+
+		return $this;
+	}
+
+	public function update() {
+
+		$userData = getUserData();
+
+		$user = self::send("users.update", "POST", array("userId" => $this->getId(), "data" => $userData));
+
+		if ( ! $user ) {
+
+			return false;
+		}
+
+		return $this;
+	}
+
+	public function info() {
+
+		$user = self::send("users.info", "GET", array("userId" => $this->getId()));
+
+		if ( ! $user ) {
+
+			return false;
+		}
+
+		$this->setName($user->user->name);
+		$this->setUserName($user->user->username);
+
+		if ( ! empty($user->user->emails) ) {
+
+			$this->setEmail($user->user->emails[0]->address);
+		}
+
+		return $this;
 	}
 
 	public function getId() {
@@ -58,9 +123,21 @@ class User extends Base {
 		return $this->id;
 	}
 
+	public function getUserName() {
+
+		return $this->userName;
+	}
+
+	public function setUserName($userName) {
+
+		$this->userName = $userName;
+
+		return $this;
+	}
+
 	public function getName() {
 
-		return $this->name;
+		return $this->Name;
 	}
 
 	public function setName($name) {
@@ -80,5 +157,152 @@ class User extends Base {
 		$this->email = $email;
 
 		return $this;
+	}
+
+	public function getActive() {
+
+		return $this->active;
+	}
+
+	public function setActive($active) {
+
+		$this->active = $active;
+
+		return $this;
+	}
+
+	public function getRoles() {
+
+		return $this->roles;
+	}
+
+	public function setRoles($roles) {
+
+		$this->roles = $roles;
+
+		return $this;
+	}
+
+	public function getJoinDefaultChannels() {
+
+		return $this->joinDefaultChannels;
+	}
+
+	public function setJoinDefaultChannels($joinDefaultChannels) {
+
+		$this->joinDefaultChannels = $joinDefaultChannels;
+
+		return $this;
+	}
+
+	public function getRequirePasswordChange() {
+
+		return $this->requirePasswordChange;
+	}
+
+	public function setRequirePasswordChange($requirePasswordChange) {
+
+		$this->requirePasswordChange = $requirePasswordChange;
+
+		return $this;
+	}
+
+	public function getSendWelcomeEmail() {
+
+		return $this->sendWelcomeEmail;
+	}
+
+	public function setSendWelcomeEmail($sendWelcomeEmail) {
+
+		$this->sendWelcomeEmail = $sendWelcomeEmail;
+
+		return $this;
+	}
+
+	public function getVerified() {
+
+		return $this->verified;
+	}
+
+	public function setVerified($verified) {
+
+		$this->verified = $verified;
+
+		return $this;
+	}
+
+	public function getCustomFields() {
+
+		return $this->customFields;
+	}
+
+	public function setCustomFields($customFields) {
+
+		$this->customFields = $customFields;
+
+		return $this;
+	}
+
+	public function getPassword() {
+
+		return $this->password;
+	}
+
+	public function setPassword($password) {
+
+		$this->password = $password;
+
+		return $this;
+	}
+
+	private function getUserData() {
+
+		$userData = array(
+			"email" => $this->email,
+			"name" => $this->name,
+			"username" => $this->userName
+		);
+
+		if ( !is_null($this->password)) {
+
+			$userData["password"] = $this->password;
+		}
+
+		if ( !is_null($this->active)) {
+
+			$userData["active"] = $this->active;
+		}
+
+		if ( !is_null($this->roles)) {
+
+			$userData["roles"] = $this->roles;
+		}
+
+		if ( !is_null($this->joinDefaultChannels)) {
+
+			$userData["joinDefaultChannels"] = $this->joinDefaultChannels;
+		}
+
+		if ( !is_null($this->requirePasswordChange)) {
+
+			$userData["requirePasswordChange"] = $this->requirePasswordChange;
+		}
+
+		if ( !is_null($this->sendWelcomeEmail)) {
+
+			$userData["sendWelcomeEmail"] = $this->sendWelcomeEmail;
+		}
+
+		if ( !is_null($this->verified)) {
+
+			$userData["verified"] = $this->verified;
+		}
+
+		if ( !is_null($this->customFields)) {
+
+			$userData["customFields"] = $this->customFields;
+		}
+
+		return $userData;
 	}
 }
