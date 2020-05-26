d2d19bfa645b438d311e0b8e199962bbaf1d1120
Sergey Aleynikov <231283+ASergey@users.noreply.github.com>
2020-05-26 19:08:24 +0300

    #27923: getMessage, update, delete methods added;

--- src/Messages/Message.php
+++ src/Messages/Message.php
@@ -13,6 +13,20 @@ class Message extends Request
     use Data;
 
     /**
+     * Gets a single chat message
+     * @return Message|bool
+     */
+    public function getMessage() {
+        static::send('chat.getMessage', 'GET', ['msgId' => $this->getMessageId()]);
+        if (!static::getSuccess()) {
+            return false;
+        }
+
+        return $this->updateOutOfResponse(static::getResponse()->message);
+    }
+
+    /**
+     * @todo if $room should be instance of Room (abstract)
      * @param Channel|Group $room
      * @return Message|bool
      */
@@ -21,11 +35,46 @@ class Message extends Request
             static::setError('Invalid room type');
             return false;
         }
-        static::send("chat.postMessage", "POST", $this->setRoomId($room->getChannelId()));
+        static::send('chat.postMessage', 'POST', $this->setRoomId($room->getChannelId()));
         if (!static::getSuccess()) {
             return false;
         }
 
         return $this->updateOutOfResponse(static::getResponse()->message);
+    }
+
+    /**
+     * Updates chat message
+     * @return Message|bool
+     */
+    public function update() {
+        static::send(
+            'chat.update',
+            'POST',
+            ['roomId' => $this->getRoomId(), 'msgId' => $this->getMessageId(), 'text' => $this->getMsg()]
+        );
+        if (!static::getSuccess()) {
+            return false;
+        }
+
+        return $this->updateOutOfResponse(static::getResponse()->message);
+    }
+
+    /**
+     * Deletes chat message
+     * @param bool $asUser Whether the message should be deleted as the user who sent it
+     * @return Message|bool
+     */
+    public function delete($asUser = false) {
+        static::send(
+            'chat.delete',
+            'POST',
+            ['roomId' => $this->getRoomId(), 'msgId' => $this->getMessageId(), 'asUser' => $asUser]
+        );
+        if (!static::getSuccess()) {
+            return false;
+        }
+
+        return $this->setMessageId(null);
     }
 }
